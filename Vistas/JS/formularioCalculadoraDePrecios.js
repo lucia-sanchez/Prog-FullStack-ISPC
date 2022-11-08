1991031d928113e2f40e8e670e1b7ae1f500e937
EmiNorte <emiliano.ram97@gmail.com>
2022-11-08 17:45:48 -0300

    subí consultas bdd

---
+++ Vistas/JS/formularioCalculadoraDePrecios.js
@@ -0,0 +1,4 @@
+const peso = document.getElementById("peso")
+const precio = document.getElementById("precio")
+const form0 = document.getElementById("form0")
+const parrafo = document.getElementById("warning")
