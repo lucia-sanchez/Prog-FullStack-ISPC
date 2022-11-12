9905337f80b94de052ea6b2f333a59b7d45c3d7f
Gonzalo Ramírez <gonzalo.emm81@gmail.com>
2022-11-11 21:57:26 -0300

    organicé archivos y linkee correctamente el js y html

--- Vistas/JS/formularioCalculadoraDePrecios.js
+++
@@ -1,4 +0,0 @@
-const peso = document.getElementById("peso")
-const precio = document.getElementById("precio")
-const form0 = document.getElementById("form0")
-const parrafo = document.getElementById("warning")
