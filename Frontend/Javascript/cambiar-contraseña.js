954210cc10059103ac87254990a86f73d7ad0f09
Lucia Sanchez <92888197+lucia-sanchez@users.noreply.github.com>
2022-11-11 23:45:31 -0300

    agregue cambiar-contraseña.js

---
+++ Frontend/Javascript/cambiar-contraseña.js
@@ -0,0 +1,84 @@
+const usuariosValidos= [{usuario:'33.545.898',
+                        password:'12345678' },
+
+                        {usuario:'25.464.878',
+                         password:'12345677' },
+
+                        {usuario:'35.698.654',
+                        password:'12345677'}, 
+
+                        {usuario:'20.656.471',
+                        password:'12345677'},
+
+                        {usuario:'34.216.873',
+                        password:'12345677'}];
+
+//PARA VALIDAR SI EL USUARIO EXISTE DEBERIA BUSCARLO EN LA BASE DE DATOS
+//COMO AÚN NO ESTA CONECTADA, LO BUSCA DENTRO DEL ARRAY DE USUARIOS VALIDOS
+
+const usuario = document.getElementById('id-usuario');
+const mail = document.getElementById('mail');
+const userError = document.getElementById('userError');
+const mensaje = document.getElementById('mensaje');
+const ingreso = document.getElementById('ingreso');
+
+ingreso.addEventListener('click', (e) => {
+    e.preventDefault();
+    validarEmpty(usuario.value, usuario, userError, 'usuario');
+    validarEmpty(mail.value, mail, mensaje, 'mail');
+    validarUsuario(usuario.value);
+    validarMail(mail.value);
+    if(validarUsuario()){
+        mensaje.innerHTML = ``;
+    }else if (usuario.value.length != 0){
+        userError.innerHTML = `<p class = "error">⚠ El usuario ingresado es incorrecto</p>`;
+    }
+    if(validarMail(mail.value)){
+        mensaje.innerHTML = `<p class= "success"> Se ha enviado un e-mail para restablecer su contaseña</p>`;
+    }
+    else{
+        mensaje.innerHTML = `<p class = "error">⚠ El mail ingresado es incorrecto</p>`;
+    }
+}); 
+function validarEmpty(valueInput, divInput, divError, nameInput){
+    if(valueInput.length == 0){
+        showError(divInput, divError, nameInput);     
+    }else{
+        hideError(divInput, divError);
+    }
+}
+/*function validarMail(mail){
+    let expReg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    let esValido = expReg.test(mail);
+    if(esValido){
+        return true;
+
+    }
+}*/
+function validarMail( email ) 
+{
+    let regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email) ? true : false;
+}
+
+function showError(divInput, divError, nameInput){
+divInput.style.border = '1px solid red';
+divError.innerHTML = `<p class = "error">⚠ Ingrese su ${nameInput}</p>`;
+}
+
+function hideError(divInput, divError){
+divInput.style.border = '1px solid black';
+divError.innerHTML = ``;
+
+} 
+function validarUsuario(){
+    let usuarioValido = false;
+    for(let i = 0; i < usuariosValidos.length; i++){
+        if(usuario.value == usuariosValidos[i].usuario){
+            usuarioValido = true;
+        }
+    }
+    return usuarioValido;
+}
+
+
