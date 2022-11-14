6b60bb6e9456b52a5e25dd36ae1c9f279b34282e
EmiNorte <emiliano.ram97@gmail.com>
2022-11-14 12:26:13 -0300

    ultimo retoque validacion

--- Frontend/Javascript/EMIformularioCalculadoraDePreciosYMás.js
+++ Frontend/Javascript/EMIformularioCalculadoraDePreciosYMás.js
@@ -1,6 +1,10 @@
 let peso = document.getElementById("peso")
 let DNI = document.getElementById("DNI")
 let DNI2 = document.getElementById("DNI2")
+let precio = document.getElementById("precio")
+let contador;
+let contador2;
+let contador3;
 
 peso.addEventListener('keypress', (event) => {
     event.preventDefault()
@@ -51,11 +55,161 @@ DNI2.addEventListener('keypress', (event) => {
 })
 
 function enviarCalculo(){
-    console.log('Calculando');
-    return false;
+
+    contador = 0
+    contador = peso.value.length
+    if (contador == 0){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        precio.placeholder="Error"
+        return false;}
+
+    else {
+        peso.value =""
+        peso.placeholder="Peso kg"
+        precio.placeholder="Calculando"
+        console.log('Calculando');
+        return false;}
 }
 
 function enviarRegistro(){
-    console.log('Enviando Registro');
-    return false;
+    
+    contador = 0
+    contador2 = 0
+    contador3 = 0
+    contador = peso.value.length
+    contador2 = DNI.value.length
+    contador3 = DNI2.value.length
+
+    if (contador != 0 && contador2 == 8 && contador3 == 8){
+        DNI.value = ""
+        DNI2.value = ""
+        peso.value = ""
+        DNI.placeholder = "DNI destinatario"
+        DNI2.placeholder = "DNI remitente"
+        peso.placeholder = "Peso kg"
+        console.log('Enviando Registro');
+        return false;}
+    else if (contador == 0 && contador2 == 8 && contador3 == 8){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        console.log('Algo salió mal');
+        return false;}
+
+    else if (contador != 0 && contador2 == 0 && contador3 == 0){
+        DNI.value=""
+        DNI.placeholder="¡Vacio! Pon 8 dígtos"
+        DNI2.value=""
+        DNI2.placeholder="¡Vacio! Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador == 0 && contador2 == 0 && contador3 == 0){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        DNI.value=""
+        DNI.placeholder="¡Vacio! Pon 8 dígtos"
+        DNI2.value=""
+        DNI2.placeholder="¡Vacio! Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+
+    else if (contador != 0 && contador2 == 8 && contador3 == 0){
+        DNI2.value=""
+        DNI2.placeholder="¡Vacio! Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador == 0 && contador2 == 8 && contador3 == 0){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        DNI2.value=""
+        DNI2.placeholder="¡Vacio! Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador != 0 && contador2 == 0 && contador3 == 8){
+        DNI.value=""
+        DNI.placeholder="¡Vacio! Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador == 0 && contador2 == 0 && contador3 == 8){
+        DNI.value=""
+        DNI.placeholder="¡Vacio! Pon 8 dígtos"
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        console.log('Algo salió mal');
+        return false;}
+
+    else if (contador != 0 && contador2 == 0 && contador3 < 8){
+        DNI.value=""
+        DNI.placeholder="¡Vacio! Pon 8 dígtos"
+        DNI2.value=""
+        DNI2.placeholder="Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador == 0 && contador2 == 0 && contador3 < 8){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        DNI.value=""
+        DNI.placeholder="¡Vacio! Pon 8 dígtos"
+        DNI2.value=""
+        DNI2.placeholder="Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador != 0 && contador2 < 8 && contador3 == 0){
+        DNI.value=""
+        DNI.placeholder="Pon 8 dígtos"
+        DNI2.value=""
+        DNI2.placeholder="¡Vacio! Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador == 0 && contador2 < 8 && contador3 == 0){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        DNI.value=""
+        DNI.placeholder="Pon 8 dígtos"
+        DNI2.value=""
+        DNI2.placeholder="¡Vacio! Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+
+    else if (contador != 0 && contador2 == 8 && contador3 < 8){
+        DNI2.value=""
+        DNI2.placeholder="Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador == 0 && contador2 == 8 && contador3 < 8){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        DNI2.value=""
+        DNI2.placeholder="Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador != 0 && contador2 < 8 && contador3 == 8){
+        DNI.value=""
+        DNI.placeholder="Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador == 0 && contador2 < 8 && contador3 == 8){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        DNI.value=""
+        DNI.placeholder="Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}    
+
+    else if (contador != 0 && contador2 < 8 && contador3 < 8){
+        DNI.value=""
+        DNI.placeholder="Pon 8 dígtos"
+        DNI2.value=""
+        DNI2.placeholder="Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;}
+    else if (contador == 0 && contador2 < 8 && contador3 < 8){
+        peso.value=""
+        peso.placeholder="¡Vacio! Pon hasta 3 dígitos"
+        DNI.value=""
+        DNI.placeholder="Pon 8 dígtos"
+        DNI2.value=""
+        DNI2.placeholder="Pon 8 dígtos"
+        console.log('Algo salió mal');
+        return false;} 
 }
